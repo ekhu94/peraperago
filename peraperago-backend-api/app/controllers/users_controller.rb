6cb6f241d8bfc988d21990b616ddd9733c8231b7
Erik Huang <erikhuangdrums@gmail.com>
2021-03-03 22:44:34 -0800

    complete seeds

--- peraperago-backend-api/app/controllers/users_controller.rb
+++ peraperago-backend-api/app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
+    end
 end
