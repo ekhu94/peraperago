2d2a9eeb966b05fe2422591e1ea6f60ec39c7a5d
Erik Huang <erikhuangdrums@gmail.com>
2021-03-08 11:32:33 -0800

    cors origin backend URL

--- peraperago-backend-api/config/initializers/cors.rb
+++ peraperago-backend-api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://ekhu94.github.io/peraperago/'
+    origins 'https://peraperago-flashcards.herokuapp.com/'
 
     resource '*',
       headers: :any,
