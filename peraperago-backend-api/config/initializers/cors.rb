b20094858b207fe3338269eef44597e8d78cfb40
Erik Huang <erikhuangdrums@gmail.com>
2021-03-08 11:23:28 -0800

    switch cors origin back to *

--- peraperago-backend-api/config/initializers/cors.rb
+++ peraperago-backend-api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://ekhu94.github.io/peraperago/'
+    origins '*'
 
     resource '*',
       headers: :any,
